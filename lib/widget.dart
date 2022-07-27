cc6ea7853490413eb4ad38fb9b4697488a95184a
ryo072144 <84168405+ryo072144@users.noreply.github.com>
2022-07-27 13:52:35 +0900

    Update and rename widget.dart to constants.dart

--- lib/widget.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-TextStyle titleStyle(){
-  return const TextStyle(fontSize: 20, color: Color(0xffeeeeee), fontWeight: FontWeight.bold);
-}
